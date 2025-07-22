fc981f37f40f1c4563ca106a380fff8f8bdb6276
Reaishma N <hayagreevar9018@gmail.com>
2025-07-22 09:12:35 +0530

    Create API.php

---
+++ routes/API.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/cloud-services', 'CloudServiceController@index');
+Route::get('/data-sources', 'DataSourceController@index');
+Route::get('/governance-metrics', 'GovernanceMetricController@index');
+Route::post('/streaming/{action}', 'StreamingController@handle');
+Route::match(['get', 'post', 'put', 'delete'], '/api-test', 'ApiTestController@handle');
